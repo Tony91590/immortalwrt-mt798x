295dc4f24449b80de1020d7d0565bb536d770113
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-09-10 21:48:05 +0300

    Update mt7986.mk

--- target/linux/mediatek/image/mt7986.mk
+++ target/linux/mediatek/image/mt7986.mk
@@ -371,11 +371,15 @@ define Device/xiaomi_redmi-router-ax6000
   DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
   DEVICE_PACKAGES := kmod-leds-ws2812b
   SUPPORTED_DEVICES := xiaomi,redmi-router-ax6000
+
   UBINIZE_OPTS := -E 5
   BLOCKSIZE := 128k
   PAGESIZE := 2048
-  IMAGE_SIZE := 112640k
+  IMAGE_SIZE := 125440k
+
   KERNEL_IN_UBI := 1
+  UBOOTENV_IN_UBI := 1
+
   IMAGES += factory.bin
   IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
